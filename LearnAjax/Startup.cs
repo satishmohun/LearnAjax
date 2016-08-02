0914813ea3fd8605ecf0b22661e57ac7cf7ff071
Satish Mohun <satishmohun@gmail.com>
2016-08-02 10:54:35 +0400

    Initial Commit + AjaxOperations

---
+++ LearnAjax/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnAjax.Startup))]
+namespace LearnAjax
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
